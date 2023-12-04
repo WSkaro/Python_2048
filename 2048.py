9236d8b533571f0ed3d91a1624bd4f1e8fd422b8
Giron Willem <147034330+WSkaro@users.noreply.github.com>
2023-12-04 15:42:05 +0100

    Update 2048.py

--- 2048.py
+++ 2048.py
@@ -9,36 +9,46 @@ def Launch_2048():
     grid_2048: List[List[int]] = [[0 for _ in range(grid_size)] for _ in range(grid_size)]
     
     show_grid(grid_2048)    
-    
-    add_tiles(grid_2048)
-    add_tiles(grid_2048)
 
     
+
     print(grid_2048)
 
 
 def show_grid(grid_2048):
-    os.system('cls' if os.name == 'nt' else 'clear')
-    for i in range(len(grid_2048)):
-        print('')
+    os.system("cls" if os.name == "nt" else "clear")
+    n = len(grid_2048)
+    for i in range(n):
+        print("♥" + "-" * (6*n-1) + "♥")
+        print("|", end="")
+        for j in range(n):
+            if grid_2048[i][j] == 0:
+                print(f"{'':^5}|", end="")
+            else:
+                print(f"{grid_2048[i][j]:^5}|", end="")
+        print()
+    print("♥" + "-" * (6*n-1)+ "♥")
+    print()
 
 
 def random_tiles():
-    """
-    prob_2: float = 9/10
-    prob_4: float = 1/10
-    """
+    tiles_probs: list[int] = []
+    prob_2: float = 9 #/10
+    prob_4: float = 1 #/10
     
-    tiles_choice: int = random.choice([2, 2, 2, 2, 2, 2, 2, 2, 2, 4])
-    return tiles_choice
+    for _ in range(prob_2):
+        tiles_probs.append(2)
+    for _ in range(prob_4):
+        tiles_probs.append(4)
+    return tiles_probs
+    
 
-
-def add_tiles(grid_2048):
-    empty: bool = [(i, j) for i in range(len(grid_2048)) for j in range(len(grid_2048)) if grid_2048[i][j] == 0]
-    if empty:
-        i, j = random.choice(empty)
-        grid_2048[i][j] = random_tiles()
-    return grid_2048
-
+def check_where_tiles_0(grid_2048):
+    for i in range(len(grid_2048)):
+        for j in range(len(grid_2048[i])):
+            if grid_2048[i][j] == 0:
+                return True
+    return False
+                
 
 Launch_2048()
