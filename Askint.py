d98831307c3586c0350e41c858b4289b58bf7be2
Giron Willem <147034330+WSkaro@users.noreply.github.com>
2023-12-04 11:41:30 +0100

    Began 2048
    Did init, reste la boucle de jeu à faire,
    btw tout en fait

---
+++ Askint.py
@@ -0,0 +1,24 @@
+chiffres: str = "0123456789"
+
+def isNumeric(s: str) -> bool:
+    """function that checks if a character in the string is a number"""
+    global chiffres
+    if s == "": return False
+    for ch in s:
+        if not ch in chiffres:
+            return False
+    return True
+
+def get_input(message: str, maximum_include=-1) -> int:
+    """function which is used for all input of the file,
+    in order to not repeat itself with the isNumeric function"""
+    valid: bool = False
+    while not valid:
+        n: str = input(message)
+        valid = isNumeric(n)
+        if not valid: 
+            print("Entrez un NOMBRE !")
+        if maximum_include != -1 and int(n) > maximum_include:
+            print("too big")
+            valid = False
+    return int(n)
--- test.py
+++ test.py
@@ -1,35 +1,20 @@
 from typing import List
 from Askint import *
 import random
-import os
  
 def Launch_2048():
     grid_size: int = get_input("Choisissez une taille de grille: ")
 
     grid_2048: List[List[int]] = [[0 for _ in range(grid_size)] for _ in range(grid_size)]
     
-    show_grid(grid_2048)    
-    
-    add_tiles(grid_2048)
-    add_tiles(grid_2048)
-
-    
     print(grid_2048)
 
-
-def show_grid(grid_2048):
-    os.system('cls' if os.name == 'nt' else 'clear')
-    for i in range(len(grid_2048)):
-        print('')
-
-
+    
 def random_tiles():
-    """
     prob_2: float = 9/10
     prob_4: float = 1/10
-    """
     
-    tiles_choice: int = random.choice([2, 2, 2, 2, 2, 2, 2, 2, 2, 4])
+    tiles_choice: int = random.choice([2, 4], weights = [prob_2, prob_4])
     return tiles_choice
 
 
